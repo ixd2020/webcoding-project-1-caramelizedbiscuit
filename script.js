420f1f34d86f59e2d6c1ec9bee897cd2f06293d5
Glitch (everything-ns) <none>
2020-12-17 03:08:21 +0000

    📖🎡 Updated with Glitch

--- script.js
+++ script.js
@@ -3,3 +3,32 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+function mobilemenu() {
+  var x = document.getElementById("navbar");
+
+  if (x.className === "navbar") {
+    x.className = "navbar responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+function loadingfinished() {
+  setTimeout(showpage, 2000);
+}
+
+function showpage() {
+  document.getElementById("preloader").style.display = "none";
+}
+
+(function() {
+  "use strict";
+  $("i").click(function() {
+    $(this)
+      .next()
+      .animate({
+        width: "250px"
+      });
+  });
+});
